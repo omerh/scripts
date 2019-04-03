69e8c15241150651cdc0f6236a9073100102daee
Omer Haim <omerha@protonmail.com>
2019-04-03 16:44:35 +0300

    Create upgrade_kops.sh

---
+++ upgrade_kops.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/"$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)"/kops-darwin-amd64
+SHA1=$(curl -L https://github.com/kubernetes/kops/releases/download/"$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)"/kops-darwin-amd64-sha1)
+
+echo "Verifying SHA1"
+
+if [[ "$SHA1" == `shasum kops | awk '{print $1}'` ]]; then
+    echo "SHA1 verifyed, replacing kops with sudo"
+    if [ -x "$(command -v kops)" ]; then
+        sudo mv ./kops `which kops`
+    else
+        sudo mv ./kops /usr/local/bin/kops
+    fi
+else
+    echo "SHA1 failed verification"
+    exit 1
+fi
