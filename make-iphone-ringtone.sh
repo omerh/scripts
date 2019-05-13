22cb2677abf59fd8c517d2a18eaabcbcfc84656f
omerh <omerha@protonmail.com>
2019-05-13 09:43:57 +0300

    fixing script

--- make-iphone-ringtone.sh
+++ make-iphone-ringtone.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# curl https://raw.githubusercontent.com/omerh/scripts/master/make-iphone-ringtone.sh | bash -s [youtube-link]
 
 pip install --upgrade youtube-dl
 brew install ffmpeg
@@ -13,6 +14,5 @@ for file in `ls`; do
   echo $file; 
   ffmpeg -t 28 -i "$file" outfile.m4a
   mv outfile.m4a ~/`echo $file|cut -d. -f1`.m4r
+  rm -f "$file"
 done
-
-rm -rf /tmp/ring/*
